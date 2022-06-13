de1c0688a21258a8337d8d703400046ddd5c9d57
Zein Kassem <zein.s.kassem@gmail.com>
2022-06-13 22:06:24 +0300

    1

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,11 @@
+import "./list.scss";
+
+const List = () => {
+  return (
+    <div className="home">
+    <h1 className="title">List</h1>
+    </div>
+  )
+}
+
+export default List
---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,11 @@
+import "./single.scss";
+
+const Single = () => {
+  return (
+    <div className="home">
+    <h1 className="title">Single page</h1>
+    </div>
+  )
+}
+
+export default Single
